9bcc4ab615d8a71673a4096e56d9e9360648764f
Newser <singkiwi@hotmail.com>
2013-08-08 22:12:37 -0400

    Added additional rankings and optimizations

--- KarmaPredictor/Base/__init__.py
+++ KarmaPredictor/Base/__init__.py
@@ -8,10 +8,56 @@ def convertCreated(integer):
     created = time.gmtime(integer)
     return created
 
-def hasKeyword(string, keywords):
-    return any(word in string for word in keywords)
+def optimize(r, opLimit):
+    av = [0, 0, 0.0]
+    i=0
+    for submission in r.get_front_page(limit=opLimit):
+        #Submission Variables
+        created = submission.created_utc
+        ups = submission.ups
+        downs = submission.downs
+        #Calculate Aditional Parameters
+        ratio = getRatio(ups, downs)
+        gap = getTimeDifference(created)
+        #DEBUG
+        #print("hours: " + str(gap) + " PPH: " + str(score / gap) + " ratio: " + str(ratio))
+        #Increment Variables
+        av[0]+=1
+        av[1]+=gap
+        av[2]+=ratio
+        #Percent Counter
+        i+=1
+        if i%(int(opLimit/10)) == 0:
+            print(str(int((i/opLimit)*100)) + "%")
+    #Calculate Averages for All Values
+    av[1] = av[1] / av[0]
+    av[2] = av[2] / av[0]
+    #Determine gap in time
+    av[1]*=0.3
+    return av[1:]
+        
+def getRatio(up, down):
+    #Up to down ratio
+    if down == 0: ratio = up
+    else: ratio = float(up) / float(down)
+    return ratio
 
-def testSubmission(submission):
+def getTimeDifference(created):
+    #Get how old post is (hours)
+    tb = convertCreated(time.time() - created)
+    tb_day = tb[2]
+    tb_hour = tb[3]
+    gap = ((tb_day - 1) * 24) + tb_hour
+    if gap < 1: gap = 1
+    return gap
+
+def getLikely(dt, up, down, comms):
+    if down < 1: down = 1
+    if dt < 1: dt = 1
+    if comms < 1: comms = 1
+    return float(1.0/float(dt)) * float(float(up)/(float(down) * 3.0)) * (20.0 / float(comms))
+    
+def testSubmission(submission, av):
     #Submission Variables
     sid = submission.id
     permalink = submission.permalink
@@ -21,35 +67,27 @@ def testSubmission(submission):
     downs = submission.downs
     num_comments = submission.num_comments
     #Determine time between post and current time
-    tb = convertCreated(time.time() - created)
-    tb_day = tb[2]
-    tb_hour = tb[3]
-    #How old post is (hours)
-    gap = ((tb_day - 1) * 24) + tb_hour
-    if gap < 1: gap = 1
-    #Up to down ratio
-    if downs == 0: 
-        ratio = ups
-    else: 
-        ratio = ups / downs
-    #If post is < 3 hrs old, points per hour > 100, and up:down >= 3:1
-    if (gap) <= 5 & int(score / gap) > 200 & int(ratio) >= 3:
-        print(" FOUND {\n" +
-            "  ID: " + str(sid) + "\n" +
-            "  SCORE: " + str(score) + "\n" +
-            "  COMMENTS: " + str(num_comments) + "\n" +
-            "  LINK: " + str(permalink) + "\n" +
-            "  Posted " + str(gap) + " hour(s)" + " ago.\n" +
-            " }"
-        )
-        doc = open('C:/Users/Daniel/Documents/GitHub/KarmaPredictor/KarmaPredictor/Base/links.txt', 'a')
-        #if str(permalink) not in doc:
-        doc.write(str(permalink) + "\n")
-        doc.close()
-        return True
+    dt = getTimeDifference(created)
+    #Compare how old post is, points per hour, and ratio of upvotes to downvotes
+    if ((dt) < av[0]) & (int(getRatio(ups, downs)) > av[1]):
+        likely = int(getLikely(dt, ups, downs, num_comments))
+        if likely > 0:
+            print(" FOUND {\n" +
+                "  LIKELY: " + str(likely) + "\n" +
+                "  ID: " + str(sid) + "\n" +
+                "  SCORE: " + str(score) + "\n" +
+                "  COMMENTS: " + str(num_comments) + "\n" +
+                "  LINK: " + str(permalink) + "\n" +
+                "  Posted " + str(dt) + " hour(s)" + " ago.\n" +
+                " }"
+            )
+            doc = open('C:/Users/Daniel/Documents/GitHub/KarmaPredictor/KarmaPredictor/Base/links.txt', 'a')
+            doc.write("[" + str(likely) + "]\t\t" + str(permalink) + "\n")
+            doc.close()
+            return True
     return False
 
-def findProspective(r, found, numPosts, startingPostLimit):
+def findProspective(r, found, numPosts, startingPostLimit, av):
     #Exceeded specs
     if (numPosts > 2000):
         print("Limit Exceeded. Stopping...")
@@ -59,28 +97,30 @@ def findProspective(r, found, numPosts, startingPostLimit):
         i+=1
         #If posts have already been covered, skip
         if (int(numPosts/2) > i) & (startingPostLimit != numPosts): pass
-        elif testSubmission(submission): found+=1
+        elif testSubmission(submission, av): found+=1
         #Percent Counter
         if i%(numPosts/10) == 0:
             print(str(int((i/numPosts)*100)) + "%")
     #If very few found, search wider specs
     if (found < 5):
         numPosts*=2
+        av[0]*=1.2
         print("Expanding Search... [posts:" + str(numPosts) + "]")
-        findProspective(r, found, numPosts, startingPostLimit)
+        findProspective(r, found, numPosts, startingPostLimit, av)
 
 #-------------------Main Script-------------------------
 found = 0
-sleep = 60
-numPosts = 50.0
-startingPostLimit = 50.0
-reqComments = 100
+opLimit = 40.0
+numPosts = 1000.0
+startingPostLimit = 1000.0
 if __name__ == "__main__":
     #Login & Connection
     r = praw.Reddit(user_agent="github/green131/karmapredictor")
     r.login()
-    keywords = [] #Keywords to check for
+    print("Opimizing Scan...")
+    av = optimize(r, opLimit)
+    print("...Opimization Complete")
     print("Beginning Scan...")
     #Find Up and Coming Submissions
-    findProspective(r, found, numPosts, startingPostLimit)
+    findProspective(r, found, numPosts, startingPostLimit, av)
     print("...Scan Complete")
